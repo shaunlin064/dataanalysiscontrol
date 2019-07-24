4ba6a788ba240143139b3e22f48b5af0706c5b20
shaun lin <shaun@js-adways.com.tw>
2019-07-24 10:03:16 +0800

    menu 提示現在頁面

--- app/Http/Middleware/CheckPermissions.php
+++ app/Http/Middleware/CheckPermissions.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Http\Controllers\Auth\Permission;
 use Closure;
+use Route;
 
 class CheckPermissions
 {
@@ -19,7 +20,23 @@ class CheckPermissions
 //	    $uid = session('userData')['user']['id'];
 	    //check permission
 	    $permission = new Permission();
-	    
+	    /**
+	     * TODO 第三層判斷展開menu 增加class name
+	     * menus_subs table 需要增加 new class 欄位
+	     *
+	     * */
+	    $permission->menus->map(function($menu,$k1){
+		    $new_class = $menu->subMenu->map(function($v,$k){
+	    		if( $v['url'] == Route::currentRouteName()){
+	    			return 'active menu-open';
+			    }
+		    })->reject(function($v,$k){
+			    return $v == null;
+		    });
+
+		    $menu['new_class'] = $new_class->first();
+		    return $menu;
+	    });
 //	    if(!in_array($uid,$permission->role->admin)){
 //
 //	      session(['menus' => $permission->menus ]) ;
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -6,10 +6,9 @@
 	 * Time: 16:05
 	 */
 
-
 ?>
 	@foreach(session('menus') as $menu)
-			<li class="treeview">
+			<li class="treeview {{ $menu['new_class'] }}">
 			<a href="#">
 				<i class='{{$menu['icon'] }}'></i>
 				 <span>{{$menu['name']}}</span>
@@ -18,13 +17,13 @@
 				 </span>
 			</a>
 			@if(count($menu->subMenu))
-				<ul class="treeview-menu {{ $menu['newclass'] }}">
+				<ul class="treeview-menu">
 					@foreach ($menu->subMenu as $sub_menu )
 						@if( session('userRoleType') == 'admin'
 						||
 						in_array( $sub_menu->id , session('role')[session('userRoleType')]['purview']['menu_subs'] ) )
-						<li class={{count($sub_menu->level2) ? 'treeview' : ''}}>
-							<a href="{{ Route($sub_menu->url) }}" target="{{$sub_menu['target']}}"
+						<li class={{count($sub_menu->level2) ? 'treeview' : ($sub_menu->url == Route::currentRouteName() ? 'active' : '')}}>
+							<a {{Route(Route::currentRouteName()) == Route($sub_menu->url) ? "class=text ":''}} href="{{ Route($sub_menu->url) }}" target="{{$sub_menu['target']}}"
 							   title="{{$sub_menu['title']}}">
 								<i class='{{$sub_menu['icon'] }}'></i> {{ $sub_menu['name'] }}
 								@if(count($sub_menu->level2))
