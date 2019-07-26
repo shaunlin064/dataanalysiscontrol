5f968b0fce1c1ead70aa9a83935bac984b323e1b
shaun lin <shaun@js-adways.com.tw>
2019-07-26 16:46:09 +0800

    新增排程 每月更新責任額

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Bonus;
+use App\BonusLevels;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +28,24 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+	    $schedule->call(function () {
+		    $thisMonth = date('Y-m-01');
+		    $lastMonth = date('Y-m-01',strtotime("-1 month"));
+		    $bonus = Bonus::where('set_date',$lastMonth)->select(['id','user_id','boundary'])->with('levels')->get();
+		    $bonus->each(function($v) use($thisMonth){
+			
+			    $v->set_date = $thisMonth;
+			    $v = $v->toArray();
+			
+			    $bonus = Bonus::create($v);
+			
+			    collect($v['levels'])->map(function($v) use($bonus){
+				    $v['bonus_id'] = $bonus->id;
+				    BonusLevels::create($v);
+			    });
+			
+		    });
+	    })->monthlyOn('1','00:00');
     }
 
     /**
