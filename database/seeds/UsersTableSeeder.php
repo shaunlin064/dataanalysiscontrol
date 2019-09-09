dd53a462a0db7aea470d4a544694fc9737f5535c
shaun lin <shaun@js-adways.com.tw>
2019-09-09 14:23:05 +0800

    修正user Seeder 為正確使用fillable參數問題

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,29 +15,37 @@
      */
     public function run()
     {
-        //
-	    DB::statement('SET FOREIGN_KEY_CHECKS=0');
-	    User::truncate();
-	    DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	    $erpUserDatas = new UserController();
 	    $erpUserDatas->getErpUser();
+	
+	    $fillable = [
+	     'name', 'email', 'password','erp_user_id'
+	    ];
+	    DB::statement('SET FOREIGN_KEY_CHECKS=0');
+	    DB::table('users')->truncate();
+	    DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	    foreach ($erpUserDatas->users as $item){
-		    $userObj = User::where('erp_user_id',$item['id'])->first();
 		
+		    $userObj = User::where('erp_user_id',$item['id'])->first();
 		    $item['name'] = $item['account'];
 		
 		    if(empty($userObj)){
-			    $request = new Request($item);
-			    $request->merge(['password'=>Hash::make($request->get('password'))]);
-			    $request->merge(['erp_user_id'=>$request->id]);
-			    User::create($request->toArray());
+			
+			    $item = collect($item);
+			    $item['password'] = Hash::make($item->get('password'));
+			    $item['erp_user_id'] = $item->get('id');
+			    $item->map(function($v,$k) use($fillable,&$item){
+				    if(!in_array($k,$fillable)){
+					    unset($item[$k]);
+				    }
+			    });
+			    User::create($item->toArray());
 			
 		    }else{
 			    $userObj->name = $item['account'];
 			    $userObj->email = $item['email'];
 			    $userObj->save();
 		    }
-		
 	    }
     }
 }
