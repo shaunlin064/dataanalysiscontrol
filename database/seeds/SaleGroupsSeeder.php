5b53441743f9dd5004076752170d8fbc196fdd8b
shaun <shaun@js-adways.com.tw>
2019-11-04 13:15:34 +0800

    更新 schedule 時間

--- database/seeds/SaleGroupsSeeder.php
+++ database/seeds/SaleGroupsSeeder.php
@@ -16,27 +16,27 @@ class SaleGroupsSeeder extends Seeder
 	    
 	    $saleGroup = [
 	     '整合行銷部' => [
-	      '2019-10-01'=>[133,153,174,188,200,201,204,205]
+	      '2019-11-01'=>[133,153,174,188,200,201,204,205],
 	     ],
 	     '業務五部' => [
 	      '2019-06-01' => [67,132,136,155],
 	      '2019-07-01' => [67,122,131,132,136,155],
 	      '2019-08-01' => [67,122,131,132,136,155],
 	      '2019-09-01' => [67,122,131,132,136,155],
-	      '2019-10-01' => [67,131,132,136,155],
+             '2019-11-01' => [67,131,132,136,155],
 	     ],
 	     '業務六部' => [
 	      '2019-06-01' => [84,156,170,181],
 	      '2019-07-01' => [84,170,181,186],
 	      '2019-08-01' => [84,170,181,186],
 	      '2019-09-01' => [84,170,181,186],
-		     '2019-10-01' => [84,170,181,186]
+          '2019-11-01' => [84,170,181,186]
 	    ],
 	     '離職業務' => [
 	      '2019-07-01' =>[97,156,161,175],
 	      '2019-08-01' =>[97,156,161,175],
 	      '2019-09-01' =>[97,156,161,175],
-	      '2019-10-01' =>[97,122,156,161,175]
+	      '2019-11-01' =>[97,122,156,161,175]
 	     ],
 	    ];
 	    $convener = [67,84];
@@ -54,7 +54,7 @@ class SaleGroupsSeeder extends Seeder
 	    foreach ($saleGroup as $name => $item){
 		
 		    $saleGroups = SaleGroups::create(['name' => $name]);
-		
+		   
 		    $dateNow = new \DateTime(date('Ym01'));
 		    
 		    $date = new \DateTime(date('2017-12-01'));
@@ -64,13 +64,18 @@ class SaleGroupsSeeder extends Seeder
 		    while($date->format('Y-m-d') != $dateNow->format('Y-m-d')){
 			    $setDate = $date->modify('+1Month')->format('Y-m-d');
 			    $nowSetDate = [];
+			    
 			    foreach ($item as $dataSetDate => $uid){
-				    if($dataSetDate == $setDate && empty($nowSetDate)) {
-					    $nowSetDate = $uid;
-				    }
-			    	if($dataSetDate > $setDate && empty($nowSetDate)) {
-					    $nowSetDate = $uid;
-				    }
+                    if(!empty($nowSetDate)) {
+                     break;
+                    }
+                    
+                    if($dataSetDate == $setDate || $dataSetDate > $setDate) {
+                        $nowSetDate = $uid;
+                    }
+                    if($dataSetDate < $setDate){
+                        $nowSetDate = last($item);
+                    }
 			    }
 			    $userData = array_map(function($v) use($setDate,$convener,$saleGroups){
 				
@@ -79,17 +84,20 @@ class SaleGroupsSeeder extends Seeder
 				     'set_date' => $setDate,
 				     'is_convener' => in_array($v,$convener) ? 1 : 0
 				    ];
+				    
 				    return $tmp;
 			    },$nowSetDate,[$setDate,$convener,$saleGroups]);
-			    $saleGroups->groupsUsers()->createMany($userData);
 			    
+       
+			    $saleGroups->groupsUsers()->createMany($userData);
+       
 			    $groupsBonus = array_map(function($v) use($setDate){
 				    $v['set_date'] = $setDate;
 				    return $v;
 			    },$groupsData,[$setDate]);
 			
 			    $saleGroups->groupsBonus()->createMany($groupsBonus);
-					  
+       
 		    };
 	    }
     }
