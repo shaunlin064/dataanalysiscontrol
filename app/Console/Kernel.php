a3ecd4eb952de8be315216fd498347f7829b1743
shaun <shaun@js-adways.com.tw>
2019-11-14 15:31:01 +0800

    Kernel use Class

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,56 +1,63 @@
 <?php
-
-namespace App\Console;
-
-use App\Console\Commands\UpdateFinancialData;
-use App\Console\Commands\UpdateSaleGroups;
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Console\Commands\UpdateUserBonus;
-
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-      UpdateUserBonus::class,
-      UpdateFinancialData::class,
-      UpdateSaleGroups::class,
-    ];
-
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-	    $schedule->command('update_financial_money_receipt')->dailyAt('00:20');
-	    $schedule->command('update_user_bonus')->monthlyOn('1','00:00');
-	    $schedule->command('update_sale_groups')->monthlyOn('1','00:10');
-	    $schedule->command('update_financial_data')->monthlyOn('16','00:00');
-	    $schedule->command('update_bonus_reach')->monthlyOn('16','00:10');
-	    $schedule->command('update_convener_reach')->monthlyOn('16','00:20');
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
-    }
     
-    protected function scheduleTimezone()
+    namespace App\Console;
+    
+    use App\Console\Commands\UpdateBonusReach;
+    use App\Console\Commands\UpdateConvenerReach;
+    use App\Console\Commands\UpdateFinancialData;
+    use App\Console\Commands\UpdateFinancialMoneyReceipt;
+    use App\Console\Commands\UpdateSaleGroups;
+    use Illuminate\Console\Scheduling\Schedule;
+    use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+    use App\Console\Commands\UpdateUserBonus;
+    
+    class Kernel extends ConsoleKernel
     {
-        return 'Asia/Taipei';
+        /**
+         * The Artisan commands provided by your application.
+         *
+         * @var array
+         */
+        protected $commands = [
+            UpdateFinancialMoneyReceipt::class,
+            UpdateUserBonus::class,
+            UpdateFinancialData::class,
+            UpdateSaleGroups::class,
+            UpdateFinancialData::class,
+            UpdateBonusReach::class,
+            UpdateConvenerReach::class,
+        ];
+        
+        /**
+         * Define the application's command schedule.
+         *
+         * @param \Illuminate\Console\Scheduling\Schedule $schedule
+         * @return void
+         */
+        protected function schedule (Schedule $schedule)
+        {
+            $schedule->command('update_financial_money_receipt')->dailyAt('00:20');
+            $schedule->command('update_user_bonus')->monthlyOn('1', '00:00');
+            $schedule->command('update_sale_groups')->monthlyOn('1', '00:10');
+            $schedule->command('update_financial_data')->monthlyOn('16', '00:00');
+            $schedule->command('update_bonus_reach')->monthlyOn('16', '00:10');
+            $schedule->command('update_convener_reach')->monthlyOn('16', '00:20');
+        }
+        
+        /**
+         * Register the commands for the application.
+         *
+         * @return void
+         */
+        protected function commands ()
+        {
+            $this->load(__DIR__ . '/Commands');
+            
+            require base_path('routes/console.php');
+        }
+        
+        protected function scheduleTimezone ()
+        {
+            return 'Asia/Taipei';
+        }
     }
-}
