87db4f01490a0a81d12d8e602d10892e89e66652
shaun <shaun.lin.064@gmail.com>
2020-04-14 10:58:49 +0800

    updateUserBonus bug fix

--- app/Console/Commands/UpdateUserBonus.php
+++ app/Console/Commands/UpdateUserBonus.php
@@ -1,94 +1,93 @@
 <?php
 
-namespace App\Console\Commands;
+    namespace App\Console\Commands;
 
-use App\Bonus;
-use App\BonusLevels;
-use App\Http\Controllers\UserController;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
+    use App\Bonus;
+    use App\BonusLevels;
+    use App\Http\Controllers\UserController;
+    use Illuminate\Console\Command;
+    use Illuminate\Support\Facades\DB;
 
-class UpdateUserBonus extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'update_user_bonus';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = '每月新增user新月份責任額';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    class UpdateUserBonus extends Command
     {
-        parent::__construct();
-    }
+        /**
+         * The name and signature of the console command.
+         *
+         * @var string
+         */
+        protected $signature = 'update_user_bonus';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        //
-        $startTime = microtime(true);
+        /**
+         * The console command description.
+         *
+         * @var string
+         */
+        protected $description = '每月新增user新月份責任額';
 
-	    $thisMonth = date('Y-m-01');
-	    $lastMonth = date('Y-m-01',strtotime("-1 month"));
-	    $bonus = Bonus::where('set_date',$lastMonth)->select(['id','erp_user_id','boundary'])->with('levels')->get();
-	    $erpUserDatas = new UserController();
-	    $erpUserDatas->getErpUser();
-	    $leaveUser = collect($erpUserDatas->users)->whereBetween('user_resign_date',[$lastMonth,date('Y-m-31',strtotime("-1 month"))])->pluck('id')->toArray();
-
-	    DB::beginTransaction();
-	    try{
-
-		    $bonus->each(function($v) use($thisMonth,$leaveUser){
-
-			    $v->set_date = $thisMonth;
-
-			    if(in_array($v->erp_user_id,$leaveUser)){
-			      $v->boundary = 0;
-			    }
-
-			    $v = $v->toArray();
-			    if(!Bonus::where('erp_user_id',$v->erp_user_id)->where('ser_date',$v->set_date)->exists){
-				    $bonus = Bonus::create($v);
-
-				    if(in_array($v['erp_user_id'],$leaveUser)) {
-					    return;
-				    }
-
-				    collect($v['levels'])->map(function ($v) use ($bonus) {
-					    $v['bonus_id'] = $bonus->id;
-					    BonusLevels::create($v);
-				    });
-			    }
-		    });
-
-		    DB::commit();
-
-	    } catch (\Exception $ex)
+        /**
+         * Create a new command instance.
+         *
+         * @return void
+         */
+        public function __construct()
         {
-		    DB::rollback();
-		    \Log::error($ex->getMessage());
-	    }
+            parent::__construct();
+        }
 
-        $runTime = round(microtime(true) - $startTime, 2);
-        echo ("Commands: {$this->signature} ({$runTime} seconds)\n");
+        /**
+         * Execute the console command.
+         *
+         * @return mixed
+         */
+        public function handle()
+        {
+            //
+            $startTime = microtime(true);
 
-        /*mail notice Job*/
-        \App\Jobs\SentMail::dispatch('crontab',['mail'=>'shaun@js-adways.com.tw','name'=>'admin', 'title' => 'update_user_bonus schedule down']);
+            $thisMonth = date('Y-m-01');
+            $lastMonth = date('Y-m-01',strtotime("-1 month"));
+            $bonus = Bonus::where('set_date',$lastMonth)->select(['id','erp_user_id','boundary'])->with('levels')->get();
+            $erpUserDatas = new UserController();
+            $erpUserDatas->getErpUser();
+            $leaveUser = collect($erpUserDatas->users)->whereBetween('user_resign_date',[$lastMonth,date('Y-m-31',strtotime("-1 month"))])->pluck('id')->toArray();
+
+            DB::beginTransaction();
+            try{
+
+                $bonus->each(function($v) use($thisMonth,$leaveUser){
+
+                    $v->set_date = $thisMonth;
+
+                    if(in_array($v->erp_user_id,$leaveUser)){
+                        $v->boundary = 0;
+                    }
+
+                    if(!Bonus::where('erp_user_id',$v->erp_user_id)->where('set_date',$v->set_date)->exists()){
+                        $bonus = Bonus::create($v->toArray());
+
+                        if(in_array($v->erp_user_id,$leaveUser)) {
+                            return;
+                        }
+                        collect($v->levels)->map(function ($v) use ($bonus) {
+                            $v->bonus_id = $bonus->id;
+
+                            BonusLevels::create($v->toArray());
+                        });
+                    }
+                });
+
+                DB::commit();
+
+            } catch (\Exception $ex)
+            {
+                DB::rollback();
+                \Log::error($ex->getMessage());
+            }
+
+            $runTime = round(microtime(true) - $startTime, 2);
+            echo ("Commands: {$this->signature} ({$runTime} seconds)\n");
+
+            /*mail notice Job*/
+            \App\Jobs\SentMail::dispatch('crontab',['mail'=>'shaun@js-adways.com.tw','name'=>'admin', 'title' => 'update_user_bonus schedule down']);
+        }
     }
-}
