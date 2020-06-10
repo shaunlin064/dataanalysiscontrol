e18142ab75331f5ca392718ab6ad37bc1a06c995
shaun <shaun.lin.064@gmail.com>
2020-06-10 11:51:59 +0800

    SaleGroups Model bug fix

--- app/SaleGroups.php
+++ app/SaleGroups.php
@@ -9,34 +9,34 @@ class SaleGroups extends Model
 {
     //
 	protected $fillable = ['name'];
-	
+
 	public function groupsUsers ()
 	{
-		
+
 			return $this->hasMany(SaleGroupsUsers::CLASS)->orderBy('set_date', 'desc')->orderBy('is_convener','desc');
-		
+
 	}
-	
+
 	public function groupsUsersLastMonth ()
 	{
 		$lastMonth = date('Y-m-01',strtotime("-1 month"));
-		$lastMonth = '2019-08-01';
+
 		return $this->hasMany(SaleGroupsUsers::CLASS)->where('set_date',$lastMonth)->orderBy('is_convener','desc');
 	}
-	
+
 	public function groupsBonus ()
 	{
 		return $this->hasMany(SaleGroupsBonusLevels::CLASS)->orderBy('set_date', 'desc');
 	}
-	
+
 	public function groupsBonusLastMonth ()
 	{
 		$lastMonth = date('Y-m-01',strtotime("-1 month"));
-		$lastMonth = '2019-08-01';
+
 		return $this->hasMany(SaleGroupsBonusLevels::CLASS)->where('set_date',$lastMonth)->orderBy('set_date', 'desc');
 	}
-	
-	
+
+
 	/**
 	 * @param $saleGroupIds
 	 * @return array
