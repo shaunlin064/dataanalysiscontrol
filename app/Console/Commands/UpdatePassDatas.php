4432ff44ec03b5fd41696a427dde30172c88357e
shaun <shaun.lin.064@gmail.com>
2020-07-28 17:13:53 +0800

    修正舊資料 放款的時間

--- app/Console/Commands/UpdatePassDatas.php
+++ app/Console/Commands/UpdatePassDatas.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Bonus;
 use App\FinancialList;
+use App\FinancialReceipt;
 use App\Provide;
 use DateTime;
 use Illuminate\Console\Command;
@@ -49,51 +50,60 @@ class UpdatePassDatas extends Command
 
         $bonuslist = FinancialList::whereNotIn('erp_user_id',$erpUSerId )
                                   ->get();
-        $fincialList = new FinancialList();
 
         ///這些之前的後勤與離職業務直接歸入已放款
-        $createdTime = new DateTime();
-        if ( $createdTime->format('d') >= 6 ) {
-            $createdTime->modify('+1Month');
-        }
-
-        $financialList = $bonuslist;
+        $finReceiptObj = new FinancialReceipt();
         //add && update
-        $financialList->map(function ( $v ) use ( $createdTime,$fincialList ) {
-            //save financialList
-            $v->status = 2;
-            $v->save();
-            $v->refresh();
+        $bonuslist->map(function ($v) use($finReceiptObj){
 
-            //calculat exchangeProfit
-            $exchangeProfitMoney = $fincialList->exchangeMoney($v)->profit;
-
-            $financial_lists_id = $v->id;
-            $bonusReach = isset($v->bonus) ? $v->bonus->bonusReach : [];
-            $bonusId = $bonusReach->bonus_id ?? 0;
-            $reachRate = $bonusReach->reach_rate ?? 0;
-            $provideMoney = $exchangeProfitMoney * $reachRate / 100;
-
-
-            $provide = Provide::where('financial_lists_id', $financial_lists_id)->first();
-
-            $provideData = [
-                'bonus_id'           => $bonusId,
-                'financial_lists_id' => $financial_lists_id,
-                'provide_money'      => $provideMoney,
-                'created_at'         => $createdTime->format('Y-m-01'),
-            ];
-
-            if ( isset($provide) ) {
-                $provide->update($provideData);
-            } else {
-                //new
-                Provide::create($provideData);
-            }
+            $finReceiptObj->providOldData($v);
 
         });
 
+//        $createdTime = new DateTime();
+//        if ( $createdTime->format('d') >= 6 ) {
+//            $createdTime->modify('+1Month');
+//        }
 
+//        $financialList = $bonuslist;
+        //add && update
+//        $financialList->map(function ( $v ) use ( $createdTime,$fincialList ) {
+//            //save financialList
+//            $v->status = 2;
+//            $v->save();
+//            $v->refresh();
+//
+//            //calculat exchangeProfit
+//            $exchangeProfitMoney = $fincialList->exchangeMoney($v)->profit;
+//
+//            $financial_lists_id = $v->id;
+//            $bonusReach = isset($v->bonus) ? $v->bonus->bonusReach : [];
+//            $bonusId = $bonusReach->bonus_id ?? 0;
+//            $reachRate = $bonusReach->reach_rate ?? 0;
+//            $provideMoney = $exchangeProfitMoney * $reachRate / 100;
+//
+//
+//            $provide = Provide::where('financial_lists_id', $financial_lists_id)->first();
+//
+//            $provideData = [
+//                'bonus_id'           => $bonusId,
+//                'financial_lists_id' => $financial_lists_id,
+//                'provide_money'      => $provideMoney,
+//                'created_at'         => $createdTime->format('Y-m-01'),
+//            ];
+//
+//            if ( isset($provide) ) {
+//                $provide->update($provideData);
+//            } else {
+//                //new
+//                Provide::create($provideData);
+//            }
+//
+//        });
+
+
+
+        /*設定責任額*/
         $userdata = $bonuslist->pluck('erp_user_id')->unique()->values()->map(function($v,$k){
             $new = [
                 'erp_user_id' => $v,
