cc1eaed7a2811f30e88bb09161f077a970977ebc
shaun <shaun.lin.064@gmail.com>
2020-07-28 17:19:40 +0800

    fix CacheAll.php 增加cache_erp_datas

--- app/Console/Commands/CacheAll.php
+++ app/Console/Commands/CacheAll.php
@@ -42,6 +42,7 @@ class CacheAll extends Command
         //
         $startTime = microtime(true);
         Artisan::call('down');
+        Artisan::Call('cache_erp_datas');
         Artisan::call('cache_clean');
         Artisan::call('cache_financial_list');
         Artisan::call('cache_receipt_times');
