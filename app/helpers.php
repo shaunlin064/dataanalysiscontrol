6172e294e3c393248a25d4aaef8bcd7a47fe451e
shaun <shaun.lin.064@gmail.com>
2020-08-14 17:11:26 +0800

    新增 helpers getFinalSql

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,15 @@
 <?php
-	/**
+
+
+    use Illuminate\Support\Str;
+
+    /**
 	 * Created by PhpStorm.
 	 * User: shaun
 	 * Date: 2019-08-15
 	 * Time: 14:34
 	 */
-	
+
 	/*START 「Date 相關」 */
 	//判斷日期為當月第幾週
 	function weekOfMonth($date) {
@@ -15,13 +19,13 @@
 	};
 	//簡單判斷日期是否是正確日期格式
 	function isDateSimple($str){
-		
+
 		if(!preg_match("/^[0-9]{4}(\-|\/)[0-9]{2}(\\1)[0-9]{2}$/", $str) && !preg_match("/^[0-9]{2}(\-|\/)[0-9]{2}$/", $str)){
 			return FALSE;
 		}
 		return TRUE;
 	}
-	
+
 	function date_range($first, $last)
 	{
 		$period = new \DatePeriod(
@@ -35,20 +39,30 @@
 		foreach ($period as $date) {
             $dates[] = $date->format('Y-m-d');
         }
-        
+
 		return $dates;
 	}
-	
+
 	/*END 「Date 相關」 */
-	
+
 	/*START 字串相關*/
 	//trim掉特殊字符
 	function escapeStr($str){
 		return preg_replace('/[\x00-\x08\x0b-\x0c\x0e-\x1f\x7f]/', '', $str);
 	}
 	/*END 字串相關*/
-	
+
     function newRequest ($Array = NULL)
     {
         return new \Illuminate\Http\Request($Array);
+    }
+
+    function getFinalSql(\Illuminate\Database\Eloquent\Builder $query)
+    {
+        $sql_str = $query->toSql();
+        $bindings = $query->getBindings();
+
+        $wrapped_str = str_replace('?', "'?'", $sql_str);
+
+        return Str::replaceArray('?', $bindings, $wrapped_str);
     }
