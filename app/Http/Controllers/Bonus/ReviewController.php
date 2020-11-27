ea2c72db3a63e6b9eaf3a949daa29b55a1a8d1ca
shaun <shaun.lin.064@gmail.com>
2020-11-27 10:52:35 +0800

    fix type miss

--- app/Http/Controllers/Bonus/ReviewController.php
+++ app/Http/Controllers/Bonus/ReviewController.php
@@ -58,7 +58,7 @@
             $medias = $objFin->getDataList('media_channel_name', 'media_channel_name');
 
             $provideObj = new ProvideController();
-            [ $saleGroups, $userList ] = $provideObj->getDataList($loginUserId, $date,'');
+            [ $saleGroups, $userList ] = $provideObj->getDataList($loginUserId, $date,'bonus_view');
 
             //            $allYearProfit = $this->getAllYearProfit($userList);
 
