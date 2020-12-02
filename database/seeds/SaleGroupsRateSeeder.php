168dd1ea329d9ca877d9b328bf3f15e2a8a1d714
shaun <shaun.lin.064@gmail.com>
2020-12-02 15:47:50 +0800

    fix: SaleGroupsRateSeeder.php 補 default 資料

--- database/seeds/SaleGroupsRateSeeder.php
+++ database/seeds/SaleGroupsRateSeeder.php
@@ -13,9 +13,29 @@ class SaleGroupsRateSeeder extends Seeder
      */
     public function run()
     {
+    	$saleGroups = \App\SaleGroups::all();
+    	
 	    /*依據 原 SaleGroupsReach 內的 rate 重建sale group rate表*/
 	    $saleGroupsReaches = \App\SaleGroupsReach::select('sale_groups_id','rate','set_date')->get()->toArray();
 	    \App\SaleGroupsRate::truncate();
 	    \App\SaleGroupsRate::insert($saleGroupsReaches);
+	    
+	    $saleGroups = \App\SaleGroups::all();
+	    
+	    foreach( date_range('20180101', today()->format('Ym01')) as $date){
+		    $saleGroups->each(function($v) use($date){
+				if(! \App\SaleGroupsRate::where(['id'=>$v->id,'set_date'=>$date])->exists()){
+					$v->saleGroupsRate()->create(
+						[
+							'set_date' => $date,
+							'rate' => 0,
+						]
+					);
+				}
+		    });
+	    }
+	    
+	    
+	    
     }
 }
